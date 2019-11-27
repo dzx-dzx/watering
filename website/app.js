ad47a472942602ad34eaaefc10393a8559670e28
dzx-dzx <dzx_dzx@hotmail.com>
2019-11-27 21:38:26 +0800

    Database written

--- website/app.js
+++ website/app.js
@@ -3,37 +3,37 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-///////////////////////////////
+//////////////////////////
 var async = require('async');
-var mongoose = require('mongoose')
-///////////////////////////////
+var fs = require('fs')
+//////////////////////////
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+var mongodb = require('mongodb')
 ///////////////////////////////
 const server = require('http').Server(express());
-
+fs.rmdir("../database", () => { });
+fs.mkdir("../database", () => { });
 var spawn = require('child_process').spawn,
   sensorProcess = spawn('python', ['../sensor/sensor.py']),
-  databaseProcess = spawn('mongod', ['--dbpath','../database/']);
+  databaseProcess = spawn('mongod', ['--dbpath', '../database/']);
+var db, client = new mongodb.MongoClient('mongodb://localhost:27017')
+client.connect(() => {
+  console.log("Connected successfully to server");
+  db = client.db('sensorData');
+});
 
 sensorProcess.stdout.on('data', function (data) {
   console.log(data.toString());
 });
-databaseProcess.stdout.on('data', function (data) {
-  console.log('Database:',data.toString());
-});
-
-
-sensorProcess.on('exit', function (code) {
-  console.log('child process exited with code ' + code.toString());
-});
+// databaseProcess.stdout.on('data', function (data) {
+//   console.log('From Database:', data.toString());
+// });
 
 const io = require('socket.io')(server);
-
 io.on('connection', (socket) => {
   var temperature, humidity;
 
@@ -43,29 +43,46 @@ io.on('connection', (socket) => {
     console.log("Server:a user go out");
   });
 
-
-
-
   function request_data() {
-    async.parallel([
-      () => {
-        socket.emit('request_temperature')
+    var temperature, humidity;
+    async.series([
+      (callback) => {
+        console.log("Now is the time:",Date.now())
+        socket.emit('request_temperature');
+        socket.emit('request_humidity');
         socket.once("response_temperature", (data) => {
-          console.log("Temperature:", data);
           temperature = data;
+          console.log("From Sensor:Temperature:", temperature);
+          callback(null, null)
         })
       },
-      () => {
-        socket.emit('request_humidity')
+      (callback) => {
         socket.once("response_humidity", (data) => {
-          console.log("Humidity:", data)
           humidity = data;
+          console.log("From Sensor:Humidity:", humidity);
+          callback(null, null)
         })
+
       },
-      (temperature, humidity) => {
-        ;
+      (callback) => {
+        console.log(`Now Storing Data ...`);
+        if (db != undefined) {
+          const collection = db.collection('documents');
+          // Insert some documents
+          collection.insertMany([
+            { temperature: temperature }, { humidity: humidity }
+          ], function (err, result) {
+            console.log(err)
+            console.log("Data Successfully Stored.");
+          })
+        }
+        callback(null, null)
       },
-      () => { setTimeout(request_data, 1000) },
+      (callback) => {
+        setTimeout(request_data, 5000);
+        callback(null, null);
+      },
+
     ])
   }
 
